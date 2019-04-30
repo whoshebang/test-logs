bc8da3ba3e8decd9c8fc3dd0cdc4a43d27ee0bfa
kaikai1024 <kaikaifeng1024@gmail.com>
2019-04-30 17:58:35 +0800

    Log format.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ extern crate log4rs;
 fn main() {
     log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
 
-    trace!("trace: booting up");
-    debug!("debug: booting up");
-    info!("info: booting up");
-    warn!("warn: booting up");
-    error!("error: booting up");
+    trace!("[topicA] booting up");
+    debug!("[topicB] booting up");
+    info!("[topicC] booting up");
+    warn!("[topicD] booting up");
+    error!("[topicE] booting up");
 
     // ...
 }
